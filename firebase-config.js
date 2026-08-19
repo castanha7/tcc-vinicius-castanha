a6fc7df2e2d4c319871d11cf4cef65e9d0023c3f
castanha7 <vinicius.calegari@escola.pr.gov.br>
2026-08-19 17:31:56 -0300

    feat: implementa autenticacao com Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 // Importa os SDKs necessários do Firebase via CDN (ES Modules)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// SUBSTITUA OS VALORES ABAIXO PELOS SEUS DADOS DO CONSOLE DO FIREBASE
+// Configuração do projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAMX16NNt07gRRb3MZCBRXxsnZVbOKKRkI",
   authDomain: "oportuna-plus.firebaseapp.com",
@@ -15,5 +16,8 @@ const firebaseConfig = {
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta o serviço de autenticação para uso na aplicação
+// Inicializa e exporta o Firebase Authentication
 export const auth = getAuth(app);
+
+// Inicializa e exporta o Cloud Firestore
+export const db = getFirestore(app);
